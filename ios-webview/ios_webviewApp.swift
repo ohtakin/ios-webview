0547b0aad0487b244c8852f9a217d8484e3a9ed1
tioh99 <ohtakin99@gmail.com>
2022-08-11 23:29:37 +0900

    Initial Commit

---
+++ ios-webview/ios_webviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_webviewApp.swift
+//  ios-webview
+//
+//  Created by TAK IN OH on 2022/08/11.
+//
+
+import SwiftUI
+
+@main
+struct ios_webviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
